7576992d7aa3b713dd1f437483790d7ce3649ea7
Tessa <tess.starkweather@gmail.com>
2023-03-27 15:39:55 -0400

    Initial Commit

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // false or 'media'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
