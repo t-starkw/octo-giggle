f0deb1ff5c06b37ecb2e45c55424e8148c1ea740
Tessa <tess.starkweather@gmail.com>
2023-03-29 14:49:12 -0400

    add content to about page

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -1,13 +1,47 @@
 import React from "react";
-
+import Typewriter from "typewriter-effect"
 
 function About() {
+    const typewrite = [
+        {
+            text: "HELLO I'M",
+            name: "TESSA STARKWEATHER",
+            post: "WEB DEVELOPER",
+            design: "BACKEND-ENGINEER",
+            desc: "this is a litte section about me",
+        },
+    ]
     return (
         <>
-            <section className="text-gray-700 h-screen flex align-center justify-center">
-                <div className="container flex flex-col items-center mx-auto md:flex-row">
-                    <h1 className="text-3xl font-large text-gray-900 dark:text-white">About</h1>
-                </div>
+            <section className='text-gray-700 flex items-center justify-center w-screen min-h-screen'>
+                {typewrite.map((val, i) => (
+                    <div className=' flex flex-col items-center mx-auto justify-center'>
+                        <div className="mb-4">
+                            <h1 className='text-2xl font-large text-gray-900 dark:text-white' data-aos='fade-right'>
+                                {val.text}
+                            </h1>
+                        </div>
+                        <div className="mb-4">
+                            <h1 className='text-5xl font-bold tracking-wider text-gray-900 dark:text-white'>
+                                <Typewriter
+                                    options={{
+                                        strings: [`${val.name}`, `${val.post}`, `${val.design}`],
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </h1>
+                        </div>
+                        <div className="mb-4 text-xl">
+                            <p data-aos='fade-left'>{val.desc}</p>
+                        </div>
+
+                        <button  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' data-aos='fade-up-right'>
+                            <a href="https://drive.google.com/file/d/1Mv03g5hAHC5_Q4j_buI-cw-VsvG6whlJ/view?usp=sharing" target="_blank">Download Resume</a>
+                            
+                        </button>
+                    </div>
+                ))}
             </section>
 
         </>
